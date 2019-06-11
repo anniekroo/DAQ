bebb539e6f617e0f4a62e76911eef4f91dec8efa
anniekroo <annie.kroo@gmail.com>
2019-06-11 12:18:18 -0400

    adding multichannel reading and non sampling time estimation

--- daq_scan.py
+++ daq_scan.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 from time import sleep
 from os import system
-from sys import stdout
+from sys import (stdout, argv)
 import array as arr
 import csv
 import time
@@ -11,7 +11,7 @@ from uldaq import (get_daq_device_inventory, DaqDevice, AInScanFlag, ScanStatus,
                    ScanOption, create_float_buffer, InterfaceType, AiInputMode)
 
 
-def main():
+def main(rate = 50000,samples_per_channel= 1000, high_channel = 1):
     daq_device = None
     ai_device = None
     status = ScanStatus.IDLE
@@ -20,11 +20,10 @@ def main():
     range_index = 1
     interface_type = InterfaceType.USB
     low_channel = 0
-    high_channel = 0
-    samples_per_channel = 10000
-    rate = 10000
+    #high_channel = 7
     scan_options = ScanOption.CONTINUOUS
     flags = AInScanFlag.DEFAULT
+    num_channels = high_channel-low_channel+1
 
     devices = get_daq_device_inventory(interface_type)
     daq_device = DaqDevice(devices[descriptor_index])
@@ -39,23 +38,24 @@ def main():
     
     # Allocate a buffer to receive the data.
     
-    data = create_float_buffer(high_channel-low_channel+1, samples_per_channel)
-    csvname = 'daq_'+str(int(rate))+'Hz_'+str(high_channel-low_channel+1)+'Ch.csv'
-    data_queue = mp.Queue(maxsize = samples_per_channel)
+    data = create_float_buffer(num_channels, samples_per_channel)
+    csvname = './data/daq_'+str(int(rate))+'Hz_'+str(num_channels)+'Ch_'+str(int(time.time()))+'.csv'
+   
+    data_queue = mp.Queue(maxsize = samples_per_channel*num_channels)
     
     with open(csvname,mode='a') as daq_file:
         daq_writer = csv.writer(daq_file)
-        rate = ai_device.a_in_scan(low_channel, high_channel, input_mode, ranges[range_index], samples_per_channel, rate, scan_options, flags, data)
+        rate = ai_device.a_in_scan(low_channel, high_channel, input_mode, ranges[range_index], samples_per_channel, rate, scan_options, flags, data)        
         i = 0
         while i <len(data):
             if data[i]!=0:
                 data_queue.put(data[i])
                 i+=1
-            if not data_queue.empty():
-                daq_writer.writerow([data_queue.get()])
-        sleep(0.01)
-        while not data_queue.empty():
-            daq_writer.writerow([data_queue.get()])
+            if data_queue.qsize()>=num_channels:
+                daq_writer.writerow([data_queue.get() for ch in range(num_channels)])
+        sleep(0.001)
+        while not data_queue.empty>=num_channels:
+            daq_writer.writerow([data_queue.get() for c in range(num_channels)])
     daq_file.close()
     
     if daq_device:
@@ -86,4 +86,7 @@ def queue_writer(data,data_queue):
 '''
         
 if __name__ == '__main__':
-    main()
+    start_time = time.time()
+    #print('DAQ Data Collection:'+argv[1]+'Hz for '+argv[2]+' samples!')
+    main(int(argv[1]),int(argv[2]))
+    print(time.time()-start_time-(float(argv[2])/float(argv[1])))
